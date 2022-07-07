f00467d8556adc5c5e77d0c1e1a234c9fab918e8
luispuentesvega <luispuentesvega@gmail.com>
2022-07-07 22:30:38 +0700

    solve spiral traverse

---
+++ src/algo/arrays/spiralTraverse.js
@@ -0,0 +1,44 @@
+// O(n) time | O(n) space where n is the total number of elements in the array
+function spiralTraverse(array) {
+  const result = [];
+  let startRow = 0;
+  let endRow = array.length - 1;
+  let startCol = 0;
+  let endCol = array[0].length - 1;
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let col = startCol; col <= endCol; col++) {
+      result.push(array[startRow][col]);
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++) {
+      result.push(array[row][endCol]);
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--) {
+      if (startRow === endRow) break;
+      result.push(array[endRow][col]);
+    }
+
+    for (let row = endRow - 1; row > startRow; row--) {
+      if (startCol === endCol) break;
+      result.push(array[row][startCol]);
+    }
+    startRow++;
+    endRow--;
+    startCol++;
+    endCol--;
+  }
+  return result;
+}
+
+const arr = [[1, 2, 3], [8, 9, 4], [7, 6, 5]];
+spiralTraverse(arr);
+/*
+
+const arr = 
+[
+  [1, 2, 3], 
+  [8, 9, 4], 
+  [7, 6, 5]];
+*/
